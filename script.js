27378b7c62907006bc599293a6a8af4bfcfd3167
Nick <Echophase44@github.com>
2022-04-06 14:36:23 -0400

    Added reset functionality

--- script.js
+++ script.js
@@ -3,15 +3,31 @@ const grid = document.querySelector(".grid");
 
 
 function makeGrid(size){
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
     for (let i = 0; i < size * size; i++){
         const gridCell = document.createElement('div');
         gridCell.classList.add("cell");
         grid.appendChild(gridCell);
-        
+    };
+}
+
+function reset(){
+    while(grid.hasChildNodes()){
+        grid.removeChild(grid.firstChild);
     }
 }
 
 test.addEventListener("click", () => {
-    makeGrid(10);
+    reset()
+    makeGrid(16);
     console.log(grid)
 });
+
+//const cell = document.querySelector(".cell")
+        
+//        cell.addEventListener("mousedown", () => {
+ //           cell.style.backgroundColor = "black";
+
+window.onload = makeGrid(16);
