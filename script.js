f0f1cf0a6d02aa4785b3ed0f02955b0d4c39b069
Nick <Echophase44@github.com>
2022-04-08 11:09:18 -0400

    Added rainbow button. Changed functionality of how color is applied to cells.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const test = document.querySelector(".test");
+
+const reset = document.querySelector(".reset");
 const grid = document.querySelector(".grid");
+const defaultBrush = document.querySelector(".blkBrush")
+const rainbow = document.querySelector(".rainbow")
+var slider = document.querySelector(".slider");
+
+let currentBrush = "default";
+const defaultGrid = 16;
+
 
 
 function makeGrid(size){
@@ -9,41 +17,52 @@ function makeGrid(size){
     for (let i = 0; i < size * size; i++){
         const gridCell = document.createElement('div');
         gridCell.classList.add("cell");
+        gridCell.addEventListener("mouseover", applyColor)
         grid.appendChild(gridCell);
     };
+    
 }
 
-function reset(){
+function removeGrid(){
     while(grid.hasChildNodes()){
         grid.removeChild(grid.firstChild);
     }
 }
 
-test.addEventListener("click", () => {
-    reset()
-    makeGrid(16);
-    modifyCell();
-    console.log(grid)
+function applyColor(e){
+    if (currentBrush === "default"){
+        e.target.style.backgroundColor = "black";
+    }
+    else if (currentBrush === "rainbowBrush"){
+        const r = Math.floor(Math.random() *256);
+        const g = Math.floor(Math.random() *256);
+        const b = Math.floor(Math.random() *256);
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+}
+
+rainbow.addEventListener("click", () => {
+    currentBrush = "rainbowBrush";
+})
+
+defaultBrush.addEventListener("click", () => {
+    currentBrush = "default"
+})
+
+slider.addEventListener("mouseup", () =>{
+    removeGrid()
+    makeGrid(`${slider.value}`);
+})
+
+reset.addEventListener("click", () => {
+    removeGrid()
+    makeGrid(`${slider.value}`);
 });
 
-function modifyCell(){
-    const cell = document.querySelectorAll(".cell");
 
-    for (let i = 0; i < cell.length; i++){
-        cell[i].addEventListener("mouseover", () => {
-        cell[i].style.backgroundColor = "black";
-     })
-    }
-};
+window.onload = makeGrid(defaultGrid);
 
-window.onload = makeGrid(16);
-
-/*TODO
--move EventListener to grid creation
--Set default settings on page load
--Set value output for slider and link with grid creation
--create handler for color selection
-    -add "rainbow" selection
--Set "reset" button to remake grid based on slider value
+/*TO DO
+-Show value of slider in HTML
 -Add some CSS, make this pig look pretty
 */
